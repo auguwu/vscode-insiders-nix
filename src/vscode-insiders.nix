67b8dbba81e2820bb503e61592510443b90e82f4
Noel Towa <cutie@floofy.dev>
2025-11-21 08:13:13 +0000

    [skip ci] Update Insiders build :sparkles:

--- src/vscode-insiders.nix
+++ src/vscode-insiders.nix
@@ -43,7 +43,7 @@
 in
   vscode.overrideAttrs (old: {
     # version comes from `package.json` of the `microsoft/vscode` repository
-    version = "1.107.20251119";
+    version = "1.107.0";
     pname = "vscode-insiders";
     src = systems.${system} or (throw "unsupported system: ${system}");
 
