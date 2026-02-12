32ece98f735aae72c08cc2762639bc796f774792
Noel Towa <cutie@floofy.dev>
2026-02-12 06:51:16 -0800

    ...it should probably run preInstall and postInstall hooks I guess

--- src/vscode-insiders/darwin.nix
+++ src/vscode-insiders/darwin.nix
@@ -16,11 +16,15 @@ vscode.overrideAttrs (old: {
     ];
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p "$out/Applications/${longName}.app" "$out/bin"
     cp -r ./* "$out/Applications/${longName}.app"
     ln -s "$out/Applications/${longName}.app/Contents/Resources/app/bin/code" "$out/bin/code-insiders"
 
     # home-manager expects `code` to be available for some reason
     ln -s "$out/Applications/${longName}.app/Contents/Resources/app/bin/code" "$out/bin/code"
+
+    runHook postInstall
   '';
 })
